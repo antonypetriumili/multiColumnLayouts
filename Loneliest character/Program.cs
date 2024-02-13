1edbaa0904ccefe0791b4957869b68b94be4578f
antonypetriumili <hokdart001@gmail.com>
2024-02-13 17:17:59 +0200

    Fix merge conflicts

---
+++ Loneliest character/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Loneliest_character
+{
+    internal class Program
+    {
+        public static char[] Loneliest(string result)
+        {
+            result = result.Trim(); // Remove leading and trailing spaces
+
+            Dictionary<char, int> spacesCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] != ' ')
+                {
+                    int left = 0, right = 0;
+
+                    int j = i - 1;
+                    while (j >= 0 && result[j] == ' ')
+                    {
+                        left++;
+                        j--;
+                    }
+
+                    j = i + 1;
+                    while (j < result.Length && result[j] == ' ')
+                    {
+                        right++;
+                        j++;
+                    }
+
+                    spacesCount[result[i]] = left + right;
+                }
+            }
+
+            int maxSpaces = spacesCount.Values.Max();
+
+            return spacesCount.Where(pair => pair.Value == maxSpaces).Select(pair => pair.Key).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Loneliest("a b  c"));
+        }
+    }
+}
